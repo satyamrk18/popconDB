689a50b039914c6b91a457ee14eb5412b3e2e401
Satyamrk <satyamrk18@gmail.com>
2025-09-16 19:08:02 +0530

    post api created for saving the movies information in database permently

--- server/index.js
+++ server/index.js
@@ -1,40 +1,72 @@
 //all imports
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors"
-
+import cors from "cors";
+import Movie from "./models/movies.js";
 //all configurations
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 //Database connection
-const connectDB = async()=>
-{
-    const connect = await mongoose.connect(process.env.MONGODB_URL);
-    if(connect)
-    {
-        console.log("Database connect successfully 🗃");
-    }
-}
-
-
+const connectDB = async () => {
+  const connect = await mongoose.connect(process.env.MONGODB_URL);
+  if (connect) {
+    console.log("Database connect successfully 🗃");
+  }
+};
 //API  requests
-app.get("/health",(req,res)=>
+
+//1] POST to add the movies in database
+
+app.post("/movies", async (req, res) => {
+  const { title, description, image, category, director, year, rating } =
+    req.body;
+
+  const newMovies = new Movie({
+    title,
+    description,
+    image,
+    category,
+    director,
+    year,
+    rating,
+  });
+
+  //saving the movie
+  const saveMovie = await newMovies.save();
+  res.json({
+    success: true,
+    sata: saveMovie,
+    message: "movie save successfully !",
+  });
+});
+
+//To get all the movies
+app.get("/movies", async(req,res)=>
 {
-    res.json({
-        success:true,
-        message:"server is healthy"
-    })
+  const movies = await Movie.find();
+
+  res.json({
+    success:true,
+    data:movies,
+    message:"movies loaded successfully !"
+  })
 })
 
+
+
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "server is healthy",
+  });
+});
 
 //server runnig
-const PORT =process.env.PORT || 8000;
-app.listen(PORT,()=>
-{
-    console.log(`server is running successfuly in port ${PORT} 🗄 `);
-    connectDB();
-})
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server is running successfuly in port ${PORT} 🗄 `);
+  connectDB();
+});
---
+++ server/models/movies.js
@@ -0,0 +1,14 @@
+import {model, Schema} from "mongoose"
+
+const moviesSchema  = new Schema({
+    title:{type:String},
+    description:{type:String},
+    image:{type:[String]},
+    category:{type:String},
+    director:{type:String},
+    year:{type:Number},
+    rating:{type:Number}
+});
+
+const Movie = model('Movie',moviesSchema)
+export default Movie;
