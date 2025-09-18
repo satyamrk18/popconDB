c7eca39def2ec1a3c83ce7c260024b1f43207de9
Satyamrk <satyamrk18@gmail.com>
2025-09-18 12:06:43 +0530

    timestamp added to handle creation and updation of the data

--- server/models/movies.js
+++ server/models/movies.js
@@ -1,20 +1,20 @@
-import {model, Schema} from "mongoose"
+import { model, Schema } from "mongoose";
 
 //schema  = means structure
 //model  = document
 
-const moviesSchema  = new Schema({
-    title:{type:String, require:true, unique: true},
-    description:{type:String, require:true},
-    image:{type:[String], require: true},
-    category:{type:String, require:true},
-    director:{type:String, require:true},
-    year:{type:Number, require:true},
-    rating:{type:Number, require:true},
-});
+const moviesSchema = new Schema(
+  {
+    title: { type: String, require: true, unique: true },
+    description: { type: String, require: true },
+    image: { type: [String], require: true },
+    category: { type: String, require: true },
+    director: { type: String, require: true },
+    year: { type: Number, require: true },
+    rating: { type: Number, require: true },
+  },
+  { timestamps: true }
+); //timestamps automatically handle the creation date, updation date of the data or document
 
-const Movie = model('Movie',moviesSchema)
+const Movie = model("Movie", moviesSchema);
 export default Movie;
-
-
-
