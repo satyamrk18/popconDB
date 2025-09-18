c86b586b755942fbeb763a24d0a1235ed8a02e2f
Satyamrk <satyamrk18@gmail.com>
2025-09-18 17:13:50 +0530

    not found page added

--- client/popcornDB/src/main.jsx
+++ client/popcornDB/src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-
-createRoot(document.getElementById('root')).render(
-  <h1>popcornDB🍿</h1>
-)
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
